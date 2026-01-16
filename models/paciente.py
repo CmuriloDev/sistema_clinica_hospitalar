791c6da822136ad19af4dc773c7f8e98eddea382
CmuriloDev <murilo.portela.dev@gmail.com>
2026-01-15 22:05:11 -0300

    Utilização da Herança para o modelo de paciente herdar o modelo de usuário

--- models/paciente.py
+++ models/paciente.py
@@ -1,14 +1,8 @@
-# Classe do paciente
-class Paciente:
-    def __init__(self, nome, cpf, email, telefone):
-        self.nome = nome
+from models.usuario import Usuario
+
+class Paciente(Usuario):
+    def __init__(self, nome, cpf, senha_hash, email, telefone, ativo=True):
+        super.__init__(nome, email, senha_hash, role="PACIENTE", ativo=ativo)
         self.cpf = cpf
-        self.email = email
         self.telefone = telefone
-
-    # Imprime as informações formatadas
-    def __str__(self):
-        cpf_formatado = f"{self.cpf[:3]}.{self.cpf[3:6]}.{self.cpf[6:9]}-{self.cpf[9:]}"
-        return f"{self.nome} (CPF: {cpf_formatado})"
-
 
